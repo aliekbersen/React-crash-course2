1fdbd3cfc0a20939a35aa8a5c258bc753921d205
RexenN <96340446+RexenN@users.noreply.github.com>
2022-08-20 14:30:08 +0300

    first

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import Header from './Header'
+import AddContact from './AddContact'
+import ContactList from './ContactList'
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <AddContact />
+      <ContactList />
+    </div>
+  );
+}
+
+export default App;
