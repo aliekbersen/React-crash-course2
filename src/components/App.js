534fc27e8d6d235dd00a5435b3df1763bb13534a
RexenN <96340446+RexenN@users.noreply.github.com>
2022-08-22 18:47:40 +0300

    app header list card

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,39 @@
 import Header from './Header'
+import { uuid } from 'uuidv4'
+import { useState, useEffect } from 'react';
 import AddContact from './AddContact'
 import ContactList from './ContactList'
 import './App.css';
 
 function App() {
+  const LOCAL_STORAGE_KEY = 'contacts'
+  const addContactHandler = (contact) => {
+    setContacts([...contacts, {id: uuid(), ...contact}])
+    
+  }
+  const [contacts, setContacts] = useState([])
+  const removeContactHandler = (id) => {
+    const newContactList = contacts.filter((contact) => {
+      return contact.id !== id;
+    })
+    setContacts(newContactList);
+  }
+
+  useEffect(() =>{
+  const retriveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+  if (retriveContacts) setContacts(retriveContacts)
+  },[])
+  
+  useEffect(() =>{
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts))
+  },[contacts])
+  
+  
   return (
-    <div>
+    <div className='ui container'>
       <Header />
-      <AddContact />
-      <ContactList />
+      <AddContact addContactHandler={addContactHandler}/>
+      <ContactList contacts={contacts} getContactId={removeContactHandler}/>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,13 @@ import React from 'react'
 
 function Header() {
   return (
-    <div>Header</div>
+    <div className='ui fixed menu'>
+        <div className='ui container center'>
+            <h2>Contact Manager</h2>
+        </div>
+
+    </div>
   )
 }
 
-export default Header
+export default Header;
